a241090cdf033bbd9ef977c507eb8636244f3ee7
123kmslvp <kriti.vanipenta@gmail.com>
2026-03-18 18:57:04 -0700

    reallly crummy edits to see if my edits work
    really bare changes to the front end main, will add more pretty things soon, but please check if this works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,14 @@ function App() {
   return (
     <Router>
       <nav style={{ padding: "10px" }}>
-        <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link> | 
+        <Link to="/login">Login</Link> |
+        <Link to="/main">Main</Link>
       </nav>
 
+      <h1> Welcome to Bracket Land</h1>
+      <h2>Join the community and compete!</h2>
+      <h3> New? <Link to="/register">Register</Link> Now</h3>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
