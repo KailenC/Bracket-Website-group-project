2dcef02b9ef17c5443a6e12658a7e5afa570a1a2
123kmslvp <kriti.vanipenta@gmail.com>
2026-03-26 00:17:15 -0700

    Main Page is better!
    yayyy more organization and a SOLID front page. I would love to add more to the browsing tournaments but i just want a good skeleton up and running for everything before fine tuning!
    
    once again im open to suggestions!!

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,19 +1,53 @@
 import { Link } from "react-router-dom";
-
 function Main() {
   return (
-    <div className="center-container">
-      <h1>Welcome to Bracket Land!!</h1>
-      <h2>Join the community and compete!</h2>
+    <div>
+      <div className="center-container">
+        <h1>Welcome to Bracket Land!!</h1>
+        <h2>Join the community and compete!</h2>
 
-      <h3>
-        New? <Link to="/register" className="btn">Register</Link> Now
-      </h3>
+        <h3>
+          New? <Link to="/register" className="btn">Register</Link> Now
+        </h3>
+      </div>
 
-      <h2>Host a Tournament!!</h2>
-      <h3>Browse Existing Tournaments</h3>
+      <br></br>
+      <br></br>
+
+      <div className="tournament-section">
+
+        <div className="tournament-header">
+        <h2>Join a Tournament!!</h2>
+        <h3>Browse Existing Tournaments</h3>
+        </div>
+
+        <div className="tournament-grid">
+            <div className="tournament-box">
+                <h4>Example Tournament 1</h4>
+                <p>UNO</p>  
+                <button className="btn"> Join! Placeholder Route</button>
+            </div>
+
+            <div className="tournament-box">
+                <h4>Example Tournament 2</h4>
+                <p>Chess</p>  
+                <button className="btn"> Join! Placeholder Route</button>
+            </div>
+
+            <div className="tournament-box">
+                <h4>Example Tournament 3</h4>
+                <p>Mario Kart</p>  
+                <button className="btn"> Join! Placeholder Route</button>
+            </div>
+        </div>
+      </div>
+    <br></br>
+        <div className="footer">
+            <p>© 2024 Bracket Land. All rights reserved.</p>
+        </div>
     </div>
+
+
   );
 }
-
 export default Main;
